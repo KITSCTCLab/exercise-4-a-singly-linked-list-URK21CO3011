758f3f9b18276e1d9161cfce4572979187a1ab97
URK21CO3011 <110014813+URK21CO3011@users.noreply.github.com>
2022-09-06 11:21:03 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,74 +1,28 @@
-from typing import Optional
+for item in input_data.split(', '):
+    inner_list = []
+    if item.isnumeric():
+        data.append(int(item))
+    elif item.startswith('['):
+        item = item[1:-1]
+        for letter in item.split(','):
+            if letter.isnumeric():
+                inner_list.append(int(letter))
+        data.append(inner_list)
 
+obj = DoublyCircularLinkedList()
+result = []
+for i in range(len(operations)):
+    if operations[i] == "add_at_head":
+        result.append(obj.add_at_head(data[i]))
+    elif operations[i] == "add_at_tail":
+        result.append(obj.add_at_tail(data[i]))
+    elif operations[i] == "add_at_index":
+        result.append(obj.add_at_index(int(data[i][0]), data[i][1]))
+    elif operations[i] == "get":
+        result.append(obj.get(data[i]))
+    elif operations[i] == "get_previous_next":
+        result.append(obj.get_previous_next(data[i]))
+    elif operations[i] == 'delete_at_index':
+        result.append(obj.delete_at_index(data[i]))
 
-class Node:
-    """
-    Provide necessary documentation
-    """
-    def __init__(self, data=None, next=None):
-        """
-        Provide necessary documentation
-        """
-        self.data = data
-        self.next = next
-
-
-class LinkedList:
-    """
-    Provide necessary documentation
-    """
-    def __init__(self):
-        """
-        Initialize the head
-        """
-        self.head = None
-
-    def insert_at_end(self, data):
-        """
-        Insert node at end of the list
-        :param data: integer data that will be used to create a node
-        """
-        # Write code here
-
-    def status(self):
-        """
-        It prints all the elements of list.
-        """
-        # write code here
-
-
-class Solution:
-    """
-    Provide necessary documentation
-    """
-    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[LinkedList]:
-        """
-        :param first_list: Linkedlist with non-negative integers
-        :param second_list: Linkedlist with non-negative integers
-        :return: returns the sum as a linked list
-        """
-        # Write code here
-        
-        
-
-# Do not edit the following code      
-# Create an instance for LinkedList
-first_list = LinkedList()
-# Create an another instance for LinkedList
-second_list = LinkedList()
-# Read data for first list
-data_for_first_list = list(map(int, input().strip().split(" ")))
-# Add data at the end of first_list
-for data in data_for_first_list:
-    first_list.insert_at_end(data)
-# Read data for second list
-data_for_second_list = list(map(int, input().strip().split(" ")))
-# Add data at the end of second_list
-for data in data_for_second_list:
-    second_list.insert_at_end(data)
-# Create an instance for Solution
-solution = Solution()
-# Pass first_list and second_list to addTwoNumbers, which returns a new linked list
-new_list = solution.addTwoNumbers(first_list, second_list)
-# Display the status of new_list
-new_list.status()
+print(result)
